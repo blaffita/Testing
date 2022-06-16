4a51bbda59b3eaf7987cb746f89c3e44efa27629
Benjamin Laffita <91285301+blaffita@users.noreply.github.com>
2022-06-16 06:52:16 -0600

    Create FirstBank.java

---
+++ BankingAppProject/FirstBank.java
@@ -0,0 +1,7 @@
+package BankingAppProject;
+public class FirstBank {
+    public static void main(String[] args) {
+        Account ben = new Account("Ben Laffita", "A00001");
+        ben.showMenu();
+    }
+}
